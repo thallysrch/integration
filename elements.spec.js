79a3ac810c0d53fc0867e433799aa701452b501f
thallysrch <52187772+thallysrch@users.noreply.github.com>
2022-01-12 11:31:04 -0300

    Add files via upload

---
+++ elements.spec.js
@@ -0,0 +1,102 @@
+/// <reference types="cypress" />
+
+const { should } = require("chai")
+
+describe('work with basic elements', () => {
+
+    //beforeEach - para executar antes de cada teste
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reloud
+    })
+
+    it('Text', () => {
+        cy.get('body').should('contain', 'Cuidado')
+        cy.get('span').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+
+    })
+
+
+    it('Links', () => {
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+        
+
+        cy.reload()
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+    })
+
+    it('Text fiels', () => {
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test')
+
+        cy.get('#elementosForm\\:sugestoes').type('textarea')
+        cy.get('#elementosForm\\:sugestoes').should('have.value', 'textarea')
+
+        //forma mais simplificada
+        //cy.get('#elementosForm\\:sugestoes')
+        //.type('textarea')
+        //.should('have value', 'textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+        .type('????') 
+        
+        cy.get('[data-cy=dataSobrenome]')
+        .type('Teste12345{backspace}{backspace}')
+        .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+        .clear()
+        .type('Erro{selectall}acerto', { delay:100 })
+        .should('have.value', 'acerto')
+    })
+
+    it('RadioButton', () =>
+    {
+        cy.get('#formSexoFem').click()
+        .should('be.checked')
+
+        cy.get('#formSexoMasc')
+        .should('not.be.checked')
+
+        cy.get("[name='formSexo']").should('have.length', 2)
+    })
+
+    it('Checkbox', () => {
+        cy.get('#formComidaPizza')
+        .click()
+        .should('be.checked')
+
+
+        cy.get("[name='formComidaFavorita']").click({multiple:true})
+        cy.get('#formComidaPizza').should('not.be.checked')
+        cy.get('#formComidaVegetariana').should('be.checked')
+    })
+
+    it.only('Combobox', () => {
+        cy.get('[data-test=dataEscolaridade]')
+        .select('2o grau completo')
+        .should('have.value', '2graucomp')
+
+        cy.get('[data-test=dataEscolaridade]')
+        .select('1graucomp')
+        .should('have.value', '1graucomp')
+
+        //validar as opções do combo
+    })
+
+    it.only ('Combo multiplo', () => {
+        cy.get('[data-testid=dataEsportes]')
+        .select(['natacao', 'Corrida', 'nada'])
+
+        //validar opções selecionadas do combo multiplo
+    })
+    
+})
